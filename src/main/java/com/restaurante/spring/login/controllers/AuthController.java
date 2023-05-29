1f51ab959dc3131cc29a495adcecf4f4eb57b29e
Rickelmy Santos Azevedo Da Cunha <125289823+RickelmySantos@users.noreply.github.com>
2023-05-28 22:13:57 -0300

    Update AuthController.java

--- src/main/java/com/restaurante/spring/login/controllers/AuthController.java
+++ src/main/java/com/restaurante/spring/login/controllers/AuthController.java
@@ -43,7 +43,7 @@ import com.restaurante.spring.login.security.services.UserDetailsImpl;
  * 
 */
 
-@CrossOrigin(origins = "https://restaurant-realeza.firebaseapp.com", maxAge = 3600)
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
